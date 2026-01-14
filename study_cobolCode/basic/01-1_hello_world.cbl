aaf119f11c4b1dd19a12ec6af3507baf60bba555
Im young <111354589+im-young@users.noreply.github.com>
2026-01-14 11:19:24 +0900

    Add files via upload

---
+++ study_cobolCode/basic/01-1_hello_world.cbl
@@ -0,0 +1,11 @@
+IDENTIFICATION DIVISION.                                     
+PROGRAM-ID. HELLO.                                            
+*
+ENVIRONMENT DIVISION.
+* 
+DATA DIVISION.
+*
+PROCEDURE DIVISION.                                          
+    DISPLAY 'HELLO WORLD'.                                   
+           MMSDEL*    STOP RUN.                                                    MFA010
+           MMSADD     GOBACK.                                                      MFA010
